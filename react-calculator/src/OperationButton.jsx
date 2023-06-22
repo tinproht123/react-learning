fb5d12d65bc433d37418eaff8d337b0c54dafcb5
tinproht123 <tinproht123@gmail.com>
2023-06-22 15:43:48 +0700

    fix UI (add icons)

--- react-calculator/src/OperationButton.jsx
+++ react-calculator/src/OperationButton.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/prop-types */
 import { ACTIONS } from "./App";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faDivide,
+  faMultiply,
+  faSubtract,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
-const OperationButton = ({ dispatch, operation }) => {
+const OperationButton = ({ dispatch, operation, icon }) => {
+  library.add(faDivide, faMultiply, faPlus, faSubtract);
   return (
     <button
       onClick={() =>
         dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
       }
     >
-      {operation}
+      {icon}
     </button>
   );
 };
