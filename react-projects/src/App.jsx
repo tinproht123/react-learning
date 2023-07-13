5288f9999a9080179fe976d487195376f6ae43af
tinproht123 <tinproht123@gmail.com>
2023-07-13 12:21:16 +0700

    finished projects

--- react-projects/src/App.jsx
+++ react-projects/src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Layout, Menu, Space, FloatButton, Drawer, Button } from "antd";
+import {
+  Layout,
+  Menu,
+  Space,
+  FloatButton,
+  Drawer,
+  Button,
+  Avatar,
+  Dropdown,
+} from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -14,6 +23,11 @@ import {
   faBars,
   faSun,
   faMoon,
+  faCaretDown,
+  faUser,
+  faCircleInfo,
+  faArrowRightFromBracket,
+  faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
 
 //import pages and projects
@@ -22,12 +36,14 @@ import ChessBoardDrawing from "./pages/chessboard-drawing";
 import Calculator from "./pages/calculator";
 import MineSweeper from "./pages/minesweeper";
 import Pomodoro from "./pages/pomodoro";
+import UserProfile from "./pages/user-profile";
 
 //import css
 import "./index.css";
 
 //import logo
 import Logo from "./assets/logo-main.svg";
+import UserAvatar from "./assets/asuka.jpg";
 
 //import Config Provider
 import { ConfigProvider } from "antd";
@@ -46,7 +62,12 @@ library.add(
   faGear,
   faBars,
   faSun,
-  faMoon
+  faMoon,
+  faCaretDown,
+  faUser,
+  faCircleInfo,
+  faArrowRightFromBracket,
+  faLocationDot
 );
 
 const getItem = (label, key, icon, children, type) => {
@@ -103,127 +124,160 @@ const items = [
   ),
 ];
 
+const menuItems = [
+  {
+    key: "0",
+    label: "User Profile",
+    icon: <FontAwesomeIcon icon="fa-solid fa-user" />,
+  },
+  {
+    key: "1",
+    label: "Settings",
+    icon: <FontAwesomeIcon icon="fa-solid fa-gear" />,
+  },
+  {
+    key: "2",
+    label: "About Us",
+    icon: <FontAwesomeIcon icon="fa-solid fa-circle-info" />,
+  },
+  {
+    type: "divider",
+  },
+  {
+    key: "4",
+    label: "Log Out",
+    icon: <FontAwesomeIcon icon="fa-solid fa-arrow-right-from-bracket" />,
+  },
+];
+
 function App() {
   const [collapsed, setCollapsed] = useState(false);
   const [open, setOpen] = useState(false);
   const [theme, setTheme] = useState("light");
-  const [language, setLanguage] = useState(zhCN);
 
   return (
     <Router>
-      <ConfigProvider locale={zhCN}>
-        {/* Settings Menu */}
-        <Drawer
-          title="Settings"
-          placement="right"
-          onClose={() => setOpen(false)}
-          open={open}
-          className={theme === "dark" ? "dark" : "light"}
+      {/* Settings Menu */}
+      <Drawer
+        title="Settings"
+        placement="right"
+        onClose={() => setOpen(false)}
+        open={open}
+        className={theme === "dark" ? "dark" : "light"}
+      >
+        <Space
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
         >
+          <h2>Theme Options</h2>
+          <Space style={{ display: "flex", alignItems: "center" }}>
+            <Button
+              icon={
+                <FontAwesomeIcon
+                  icon="fa-solid fa-sun"
+                  size="xl"
+                  color={theme === "light" ? "#1677ff" : "#fff"}
+                />
+              }
+              onClick={() => {
+                setTheme("light");
+              }}
+              size="large"
+              style={{ backgroundColor: "transparent !important" }}
+              className={`${theme === "dark" && "btn-dark"}`}
+            >
+              Light
+            </Button>
+            <Button
+              icon={
+                <FontAwesomeIcon
+                  icon="fa-solid fa-moon"
+                  size="xl"
+                  color={theme === "dark" ? "#1677ff" : ""}
+                />
+              }
+              onClick={() => setTheme("dark")}
+              size="large"
+              style={{ backgroundColor: "transparent !important" }}
+              className={`${theme === "dark" && "btn-dark"}`}
+            >
+              Dark
+            </Button>
+          </Space>
+        </Space>
+      </Drawer>
+      {/* Float settings button */}
+      <FloatButton
+        icon={
+          <FontAwesomeIcon
+            icon="fa-solid fa-gear"
+            style={{ cursor: "pointer" }}
+          />
+        }
+        type="primary"
+        style={{ right: 24 }}
+        onClick={() => setOpen(true)}
+        tooltip={<div>Settings</div>}
+      />
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider collapsed={collapsed} style={{ width: "300px" }}>
           <Space
             style={{
               display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "64px",
             }}
           >
-            <h2>Theme Options</h2>
-            <Space style={{ display: "flex", alignItems: "center" }}>
-              <Button
-                icon={
-                  <FontAwesomeIcon
-                    icon="fa-solid fa-sun"
-                    size="xl"
-                    color={theme === "light" ? "#1677ff" : "#fff"}
-                  />
-                }
-                onClick={() => {
-                  setTheme("light");
-                }}
-                size="large"
-                style={{ backgroundColor: "transparent !important" }}
-                className={`${theme === "dark" && "btn-dark"}`}
-              >
-                Light
-              </Button>
-              <Button
-                icon={
-                  <FontAwesomeIcon
-                    icon="fa-solid fa-moon"
-                    size="xl"
-                    color={theme === "dark" ? "#1677ff" : ""}
-                  />
-                }
-                onClick={() => setTheme("dark")}
-                size="large"
-                style={{ backgroundColor: "transparent !important" }}
-                className={`${theme === "dark" && "btn-dark"}`}
-              >
-                Dark
-              </Button>
-            </Space>
+            <img src={Logo} width="50" style={{ fill: "#000" }} />
           </Space>
-        </Drawer>
-        {/* Float settings button */}
-        <FloatButton
-          icon={
+          <Menu
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["app"]}
+            mode="inline"
+            theme={theme === "dark" ? "dark" : "light"}
+            items={items}
+            style={{ height: "100%" }}
+          />
+        </Sider>
+        <Layout>
+          <Header className={`header ${theme === "light" && "light"}`}>
             <FontAwesomeIcon
-              icon="fa-solid fa-gear"
-              style={{ cursor: "pointer" }}
+              icon="fa-solid fa-bars"
+              size="xl"
+              style={{ transition: "0.5 all ease", cursor: "pointer" }}
+              rotation={collapsed ? 0 : 90}
+              onClick={() => setCollapsed((prev) => !prev)}
             />
-          }
-          type="primary"
-          style={{ right: 24 }}
-          onClick={() => setOpen(true)}
-          tooltip={<div>Settings</div>}
-        />
-        <Layout style={{ minHeight: "100vh" }}>
-          <Sider collapsed={collapsed} style={{ width: "300px" }}>
-            <Space
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "64px",
-              }}
-            >
-              <img src={Logo} width="50" style={{ fill: "#000" }} />
+            <Space>
+              <Space>
+                <Link to="/user-profile">
+                  <p style={{ fontSize: "16px" }}>Khánh Tùng</p>
+                </Link>
+                <FontAwesomeIcon icon="fa-solid fa-caret-down" />
+              </Space>
+              <Avatar src={UserAvatar} size={40} />
             </Space>
-            <Menu
-              defaultSelectedKeys={["1"]}
-              defaultOpenKeys={["app"]}
-              mode="inline"
-              theme={theme === "dark" ? "dark" : "light"}
-              items={items}
-              style={{ height: "100%" }}
-            />
-          </Sider>
-          <Layout>
-            <Header className={`header ${theme === "light" && "light"}`}>
-              <FontAwesomeIcon
-                icon="fa-solid fa-bars"
-                size="xl"
-                style={{ transition: "0.5 all ease", cursor: "pointer" }}
-                rotation={collapsed ? 0 : 90}
-                onClick={() => setCollapsed((prev) => !prev)}
-              />
-            </Header>
+          </Header>
 
-            <Content style={{ padding: "20px" }}>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route
-                  path="/chessboard-drawing"
-                  element={<ChessBoardDrawing />}
-                />
-                <Route path="/calculator" element={<Calculator />} />
-                <Route path="/minesweeper" element={<MineSweeper />} />
-                <Route path="/pomodoro" element={<Pomodoro />} />
-              </Routes>
-            </Content>
-          </Layout>
+          <Content style={{ padding: "20px" }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/chessboard-drawing"
+                element={<ChessBoardDrawing />}
+              />
+              <Route path="/calculator" element={<Calculator />} />
+              <Route path="/minesweeper" element={<MineSweeper />} />
+              <Route path="/pomodoro" element={<Pomodoro />} />
+              <Route path="/user-profile" element={<UserProfile />} />
+            </Routes>
+          </Content>
         </Layout>
-      </ConfigProvider>
+      </Layout>
     </Router>
   );
 }
---
+++ react-projects/src/pages/user-profile/index.jsx
@@ -0,0 +1,417 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/jsx-no-target-blank */
+import { Space, Row, Col, Divider, Rate, Tag } from "antd";
+import UserAvatar from "../../assets/asuka.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+const UserProfile = () => {
+  return (
+    <div
+      style={{
+        color: "#333 !important",
+        padding: "20px",
+        borderRadius: "5px",
+        boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+      }}
+    >
+      <h1 style={{ marginBottom: "30px", fontSize: "30px" }}>Profile</h1>
+      <Row style={{ display: "flex", justifyContent: "space-between" }}>
+        <Col
+          span={5}
+          style={{
+            padding: "8px",
+          }}
+        >
+          <img src={UserAvatar} style={{ width: "100%" }} alt="avatar" />
+          <div style={{ marginBottom: "40px" }}>
+            <Divider
+              orientation="left"
+              orientationMargin="0"
+              style={{ borderBlockColor: "#ccc" }}
+            >
+              <p
+                style={{
+                  color: "#999",
+                  fontWeight: "bold",
+                  letterSpacing: "2px",
+                  fontSize: "18px",
+                }}
+              >
+                Education
+              </p>
+            </Divider>
+            <div style={{ marginBottom: "20px" }}>
+              <p
+                style={{ fontSize: "16px", color: "#666", fontWeight: "bold" }}
+              >
+                08/2020 - 06/2023
+              </p>
+              <h5
+                style={{
+                  fontSize: "17px",
+                  color: "#444",
+                  fontWeight: "bold",
+                  margin: "5px 0",
+                }}
+              >
+                HNIVC
+              </h5>
+              <Tag color="#1677ff" style={{ fontSize: "16px" }}>
+                Application Software Developer
+              </Tag>
+              <ul
+                style={{
+                  fontSize: "16px",
+                  marginTop: "10px",
+                  marginLeft: "10px",
+                }}
+              >
+                <li>Computer Science</li>
+                <li>Build a Managment System on ASP.NET MVC</li>
+                <li>Web Dev and Web Design</li>
+              </ul>
+            </div>
+            <div style={{ marginBottom: "20px" }}>
+              <p
+                style={{ fontSize: "16px", color: "#666", fontWeight: "bold" }}
+              >
+                09/2022 - 12/2022
+              </p>
+              <h5
+                style={{
+                  fontSize: "17px",
+                  color: "#444",
+                  fontWeight: "bold",
+                  margin: "5px 0",
+                }}
+              >
+                PTIT
+              </h5>
+              <Tag color="#1677ff" style={{ fontSize: "16px" }}>
+                Web Developer
+              </Tag>
+              <ul
+                style={{
+                  fontSize: "16px",
+                  marginTop: "10px",
+                  marginLeft: "10px",
+                }}
+              >
+                <li>Build a RESTFUL API with Spring Boot and MySql</li>
+                <li>Build a complete E-Commerce Website</li>
+                <li>Web Dev and Web Design</li>
+              </ul>
+            </div>
+          </div>
+          <div style={{ marginBottom: "40px" }}>
+            <Divider
+              orientation="left"
+              orientationMargin="0"
+              style={{ borderBlockColor: "#ccc" }}
+            >
+              <p
+                style={{
+                  color: "#999",
+                  fontWeight: "bold",
+                  letterSpacing: "2px",
+                  fontSize: "18px",
+                }}
+              >
+                Works
+              </p>
+            </Divider>
+            <div style={{ marginBottom: "20px" }}>
+              <p
+                style={{
+                  fontSize: "16px",
+                  color: "#666",
+                  fontWeight: "bold",
+                }}
+              >
+                12/2023 - 05/2023
+              </p>
+              <h5
+                style={{
+                  fontSize: "17px",
+                  color: "#444",
+                  fontWeight: "bold",
+                  margin: "5px 0",
+                }}
+              >
+                SIMAX
+              </h5>
+              <Tag color="#cd201f" style={{ fontSize: "16px" }}>
+                Intern
+              </Tag>
+              <ul
+                style={{
+                  fontSize: "16px",
+                  marginTop: "10px",
+                  marginLeft: "10px",
+                }}
+              >
+                <li>Design UI/UX for company's website</li>
+                <li>Develop a Managment System on ASP.NET MVC</li>
+                <li>Company's application testing</li>
+              </ul>
+            </div>
+          </div>
+        </Col>
+        <Col span={18}>
+          <Space>
+            <h2
+              style={{
+                fontSize: "28px",
+                letterSpacing: "1px",
+                color: "#555",
+                marginBottom: "10px",
+              }}
+            >
+              Nguyễn Gia Khánh Tùng
+            </h2>
+            <Space style={{ color: "#999" }}>
+              <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+              <p>Hanoi, Vietnam</p>
+            </Space>
+          </Space>
+          <h3
+            style={{
+              color: "#2196f3",
+              fontWeight: "bold",
+              letterSpacing: "2px",
+            }}
+          >
+            Web Developer
+          </h3>
+          <div style={{ marginTop: "40px" }}>
+            <Divider
+              orientation="left"
+              orientationMargin="0"
+              style={{ borderBlockColor: "#ccc" }}
+            >
+              <p
+                style={{
+                  color: "#999",
+                  fontWeight: "bold",
+                  letterSpacing: "2px",
+                  fontSize: "18px",
+                }}
+              >
+                Rankings
+              </p>
+            </Divider>
+            <Space style={{ display: "flex", alignItems: "center" }}>
+              <p style={{ fontSize: "24px", fontWeight: "bold" }}>8.6</p>
+              <Rate
+                allowHalf
+                disabled
+                defaultValue={4.5}
+                style={{ color: "#2196f3" }}
+              />
+            </Space>
+          </div>
+          <div style={{ marginTop: "40px" }}>
+            <Divider
+              orientation="left"
+              orientationMargin="0"
+              style={{ borderBlockColor: "#ccc" }}
+            >
+              <p
+                style={{
+                  color: "#999",
+                  fontWeight: "bold",
+                  letterSpacing: "2px",
+                  fontSize: "18px",
+                }}
+              >
+                About Me
+              </p>
+            </Divider>
+            <div>
+              <div style={{ marginBottom: "30px" }}>
+                <h4
+                  style={{
+                    color: "#555",
+                    marginBottom: "20px",
+                    fontSize: "16px",
+                    letterSpacing: "2px",
+                  }}
+                >
+                  CONTACT INFORMATION
+                </h4>
+                <Row style={{ marginBottom: "10px" }}>
+                  <Col span={2}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Phone:
+                    </p>
+                  </Col>
+                  <Col span={22}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        color: "#2196f3",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      +84 0924 808 757
+                    </p>
+                  </Col>
+                </Row>
+                <Row style={{ marginBottom: "10px" }}>
+                  <Col span={2}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Address:
+                    </p>
+                  </Col>
+                  <Col span={22}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      18 Linh Nam, Vinh Hung, Hoang Mai, Hanoi, Vietnam
+                    </p>
+                  </Col>
+                </Row>
+                <Row style={{ marginBottom: "10px" }}>
+                  <Col span={2}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Email
+                    </p>
+                  </Col>
+                  <Col span={22}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        color: "#2196f3",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      <a href="mailto:tinproht123@gmail.com?subject=Example%20Subject&amp;body=Example%20Body">
+                        tinproht123@gmail.com
+                      </a>
+                    </p>
+                  </Col>
+                </Row>
+                <Row style={{ marginBottom: "10px" }}>
+                  <Col span={2}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Site
+                    </p>
+                  </Col>
+                  <Col span={22}>
+                    <p
+                      style={{
+                        color: "#2196f3",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      <a
+                        href="https://khanh-tung-portfolio.w3spaces.com/"
+                        target="_blank"
+                      >
+                        https://khanh-tung-portfolio.w3spaces.com/
+                      </a>
+                    </p>
+                  </Col>
+                </Row>
+              </div>
+              <div style={{ marginBottom: "30px" }}>
+                <h4
+                  style={{
+                    color: "#555",
+                    marginBottom: "20px",
+                    fontSize: "16px",
+                    letterSpacing: "2px",
+                  }}
+                >
+                  BASIC INFORMATION
+                </h4>
+                <Row style={{ marginBottom: "10px" }}>
+                  <Col span={2}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Birthday:
+                    </p>
+                  </Col>
+                  <Col span={22}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      January 30, 2002
+                    </p>
+                  </Col>
+                </Row>
+                <Row style={{ marginBottom: "10px" }}>
+                  <Col span={2}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Gender:
+                    </p>
+                  </Col>
+                  <Col span={22}>
+                    <p
+                      style={{
+                        fontWeight: "bold",
+                        letterSpacing: "1px",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Male
+                    </p>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default UserProfile;
